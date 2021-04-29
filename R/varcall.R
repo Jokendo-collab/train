2aca36edb9fa0ef38a0c6c91f3d0b35659f03511
Javan Okendo <javanokendo@gmail.com>
2021-04-29 15:20:23 +0300

    updated readme

--- R/varcall.R
+++ R/varcall.R
@@ -1,5 +1,15 @@
 
+#' This is the package that is used in the analysis of gene sets
+#'
+#' @param gsea
+#'
+#' @return gene sets indentified
+#' @export
+#'
+#' @examples
 splt = function(gsea){
+
   x + y + 1
-  return("The value is:", gsea)
+  return(gsea)
 }
+
