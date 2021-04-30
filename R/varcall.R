e03258a1fdec9384d09b0610ed7ab36d91bb29e4
Javan Okendo <javanokendo@gmail.com>
2021-04-30 09:01:05 +0300

    updated the script code

--- R/varcall.R
+++ R/varcall.R
@@ -1,15 +1,22 @@
 
 #' This is the package that is used in the analysis of gene sets
 #'
-#' @param gsea
+#' @param n
 #'
 #' @return gene sets indentified
 #' @export
 #'
 #' @examples
-varcall = function(n){
+varcall = function(x,y){
   #compute the gene expression profile
-  n^25
-
+  x = 5000
+  y = 60000
+  z = x+y
+  for (i in z) {
+    b <- i^56
+    print(b)
+    return(i)
+    }
 }
+varcall(23,34)
 
